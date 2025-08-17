7673f1b4787b71f9836e1a97bee06a9f46d29c3b
Kaio Vitor Santos Queiroz <123708201+Kaio-0708@users.noreply.github.com>
2025-08-17 11:30:50 -0300

    Update db.js

--- projeto/config/db.js
+++ projeto/config/db.js
@@ -7,27 +7,23 @@ const dbName = process.env.MONGO_DB;
 
 const uri = `mongodb+srv://${user}:${password}@cluster0.aa30dnf.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 
-let client;
-let clientPromise;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  clientPromise = client.connect();
-  global._mongoClientPromise = clientPromise;
-} else {
-  clientPromise = global._mongoClientPromise;
+async function connect() {
+  try {
+    await client.connect();
+    console.log("MongoDB Atlas conectado!");
+    const db = client.db(dbName);
+    return db;
+  } catch (err) {
+    console.error("Erro ao conectar:", err);
+  }
 }
 
-async function getCollection() {
-  const client = await clientPromise;
-  const db = client.db(dbName);
-  return db.collection("usuarios");
-}
-
-module.exports = getCollection;
+module.exports = connect;
